e736d9e2516d9c9ce254f26116d571139b0864ba
Cristhian Garcia <crisgarta8@gmail.com>
2023-04-17 13:57:08 -0500

    fix: remove port from oauth when https is enabled

--- tutorsuperset/templates/superset/jobs/init/init-openedx.sh
+++ tutorsuperset/templates/superset/jobs/init/init-openedx.sh
@@ -2,7 +2,7 @@
 ./manage.py lms manage_user superset superset@apache
 ./manage.py lms create_dot_application \
     --grant-type authorization-code \
-    --redirect-uris "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ SUPERSET_HOST }}:{{ SUPERSET_PORT }}/oauth-authorized/openedxsso" \
+    --redirect-uris "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ SUPERSET_HOST }}{% if not ENABLE_HTTPS %}:{{SUPERSET_PORT}}{% else %}http{% endif %}/oauth-authorized/openedxsso" \
     --client-id {{ SUPERSET_OAUTH2_CLIENT_ID }} \
     --client-secret {{ SUPERSET_OAUTH2_CLIENT_SECRET }} \
     --scopes "user_id" \
